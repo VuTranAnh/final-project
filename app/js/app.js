32dd83de05584ef03f0a8452999bbf7196c3c271
TranAnhVu <vuta@asiantech.vn>
2016-06-27 14:30:01 +0700

    create angular app, create about controller and add route for about page

--- app/js/app.js
+++ app/js/app.js
@@ -1,2 +1,14 @@
 'use strict';
+var myApp = angular.module('myApp', [
+	'ngRoute',
+	'myControllers'
+	]);
 
+myApp.config(['$routeProvider', function ($routeProvider) {
+	$routeProvider
+		.when('/about', {
+			templateUrl: 'view/about.html',
+			controller: 'AboutController'
+		})
+		.otherwise({redirectTo: '/about'});
+}]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,2 +1,9 @@
 'use strict';
 
+var myControllers = angular.module('myControllers', ['ngRoute']);
+
+myControllers.controller('AboutController', ['$scope', '$route', 
+	function ($scope, $route) {
+		$scope.template = $route.current.templateUrl;	
+	}
+]);
